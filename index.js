156ef51884a1a0633f4c84d4483c6ba4fb4cc7bd
kruthik693 <kruthik693@gmail.com>
2025-12-12 15:16:03 +0530

    progress

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello, my name is kruthik i am attending React js class form past a week in the instuite called xwrokz-obc and i am enjoying the class very much and learning new things every day");
+console.log("Hello, my name is kruthik i am attending React js class form past a week  in the instuite called xwrokz-obc and\n i am enjoying the class very much and learning new things every day");
+
+// *******************function**********************
+// function calcuate(a,b){
+    
+//     console.log(a+b);
+
+// }
+// calcuate(10,20);
+// ***************calbackfunction*************
+function display(name, age){
+    console.log("name:", name);
+    age();
+}
+function tweetyear(){
+    console.log("i am 20 years old");
+}
+display("kruthik", tweetyear);
