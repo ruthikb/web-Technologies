3afe2d4a11fbe56ed1a61b4123c254e40a2ff7a9
kruthik693 <kruthik693@gmail.com>
2025-12-16 21:52:29 +0530

    first code of react

---
+++ react-app.js
@@ -0,0 +1,5 @@
+var heading = React.createElement("h1", {}, "Hello, World!");
+console.log(heading);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
+root.render(heading);
