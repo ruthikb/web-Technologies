20e21d24a9021f20eb58bde7196f80027d7a103d
kruthik693 <kruthik693@gmail.com>
2025-12-16 16:02:18 +0530

    domUpdated

---
+++ dom.js
@@ -0,0 +1,4 @@
+document.getElementById("heading").innerHTML="<p style='color: red;'> Hello World </p>";
+var heading = document.createElement("h2");
+heading.innerText="This is created using DOM";
+document.body.appendChild(heading);
